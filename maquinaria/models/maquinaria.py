23b593638f24ed88d9b6763bc0e441814f3fc056
vici0uz <sasorinocho@mixmail.com>
2020-11-04 17:48:42 -0300

    beta

--- maquinaria/models/maquinaria.py
+++ maquinaria/models/maquinaria.py
@@ -11,15 +11,15 @@ class maquina(models.Model):
     # operador = fields.Many2one(comodel_name='res.partner')
     # marca = fields.Many2one(comodel_name='maquinaria.maquina.marca')
     modelo = fields.Many2one(comodel_name='maquinaria.maquina.modelo')
-    lugar_de_trabajo = fields.Many2one(comodel_name='maquinaria.destino')
+    # lugar_de_trabajo = fields.Many2one(comodel_name='maquinaria.destino')
     trabajo_lineas_ids = fields.One2many(comodel_name='maquinaria.trabajo.linea', inverse_name='maquina_id')
     no_serie = fields.Char(string='Nro de serie', required=True)
 
     #Computados
-    ultimo_odometro = fields.Integer(compute='_set_last_data')
-    ultimo_lugar = fields.Many2one(compute='_set_last_data')
-    ultimo_operador = fields.Many2one(comodel_name='res.partner', compute='_set_last_data')
-    ultima_foto_odometro = fields.Binary(compute='_set_last_data')
+    ultimo_odometro = fields.Integer()
+    ultimo_lugar = fields.Many2one(comodel_name='maquinaria.destino')
+    ultimo_operador = fields.Many2one(comodel_name='res.partner')
+    ultima_foto_odometro = fields.Binary()
 
     @api.multi
     @api.depends('modelo', 'no_serie')
@@ -28,16 +28,38 @@ class maquina(models.Model):
             if record.no_serie and record.modelo:
                 record.name = record.modelo.name + '/ ' + record.no_serie
 
+    # @api.multi
+    # @api.depends('trabajo_lineas_ids')
+    # def _set_last_data(self):
+    #     for record in self:
+    #         if record.trabajo_lineas_ids:
+    #             ultimo_trabajo = self.env['maquinaria.trabajo.linea'].search([('maquina_id', '=', record.id)], order='id desc', limit=2)[0]
+    #             print(col.red('ALAN DEBUG: ' + str(ultimo_trabajo)))
+    #             print(col.red('ALAN DEBUG: ' + str(ultimo_trabajo.odometro)))
+    #             record.ultimo_odometro = ultimo_trabajo.odometro
+    #             record.ultimo_operador = ultimo_trabajo.operador.id
+    #             record.ultimo_lugar = ultimo_trabajo.trabajo_destino.id
+    #             record.ultima_foto_odometro = ultimo_trabajo.odometro_imagen
+
     @api.multi
-    @api.depends('trabajo_lineas_ids')
-    def _set_last_data(self):
+    def launch_wizard(self):
         for record in self:
-            if record.trabajo_lineas_ids:
-                ultimo_trabajo = record.trabajo_lineas_ids[:-1]
-                record.ultimo_odometro = ultimo_trabajo.odometro
-                record.ultimo_operador = ultimo_trabajo.operador.id
-                record.ultimo_lugar =  ultimo_trabajo.trabajo_destino.id
-                record.ultima_foto_odometro = ultimo_trabajo.odometro_imagen
+            view = self.env.ref('maquinaria.wizard')
+            return {
+                'type': 'ir.actions.act_window',
+                'name': 'Registrar trabajo',
+                'res_model': 'maquinaria.wizard',
+                'view_type': 'form',
+                'view_mode': 'form',
+                'view_id': view.id,
+                'target': 'new',
+                'context': {
+                    'current_id': self.id,
+                }
+            }
+            {
+
+            }
 
 
 class maquina_marca(models.Model):
@@ -72,7 +94,39 @@ class maquinaria_trabajo(models.Model):
 
     maquina_id = fields.Many2one(comodel_name='maquinaria.maquina', ondelete='restrict')
     odometro = fields.Integer()
-    odometro_imagen = fields.Binary(string='Odometro', attachment=True)
+    odometro_imagen = fields.Binary(string='Foto del Odometro', attachment=True)
     trabajo_destino = fields.Many2one(comodel_name='maquinaria.destino', string='Lugar')
     operador = fields.Many2one(comodel_name='res.partner')
     fecha_trabajo = fields.Date(default=fields.Date.context_today, string="Fecha")
+
+class maquinaria_wizard(models.TransientModel):
+    _name = 'maquinaria.wizard'
+
+    maquina_id = fields.Many2one(comodel_name='maquinaria.maquina', ondelete='restrict')
+    odometro = fields.Integer()
+    odometro_imagen = fields.Binary(string='Foto del Odometro', attachment=True)
+    trabajo_destino = fields.Many2one(comodel_name='maquinaria.destino', string='Lugar')
+    operador = fields.Many2one(comodel_name='res.partner')
+    fecha_trabajo = fields.Date(default=fields.Date.context_today, string="Fecha")
+
+    @api.multi
+    def guardar_datos(self):
+        for record in self:
+            if self.env.context['current_id']:
+                linea = self.env['maquinaria.trabajo.linea']
+                maquina = self.env['maquinaria.maquina']
+                linea_data = {
+                    # 'maquina_id': self.env.context['current_id'],
+                    'odometro': record.odometro,
+                    'odometro_imagen': record.odometro_imagen,
+                    'fecha_trabajo': record.fecha_trabajo,
+                    'operador': record.operador.id,
+                    'trabajo_destino': record.trabajo_destino.id
+                }
+                maquina.browse(self.env.context['current_id']).write({
+                    'ultimo_odometro': record.odometro,
+                    'ultimo_lugar': record.trabajo_destino.id,
+                    'ultimo_operador': record.operador.id,
+                    'ultima_foto_odometro': record.odometro_imagen,
+                    'trabajo_lineas_ids': [(0, 0, linea_data)]
+                    })
