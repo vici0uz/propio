c0a8a16baaf42d3d966e0d0d4c79151d6137954d
vici0uz <sasorinocho@mixmail.com>
2020-11-04 16:34:35 -0300

    beta

--- maquinaria/models/maquinaria.py
+++ maquinaria/models/maquinaria.py
@@ -5,12 +5,39 @@ from clint.textui import colored as col
 class maquina(models.Model):
     _name = 'maquinaria.maquina'
 
+
     responsable = fields.Many2one(comodel_name='res.partner')
-    operador = fields.Many2one(comodel_name='res.partner')
+    name = fields.Char(compute='_set_name', store=True)
+    # operador = fields.Many2one(comodel_name='res.partner')
     # marca = fields.Many2one(comodel_name='maquinaria.maquina.marca')
     modelo = fields.Many2one(comodel_name='maquinaria.maquina.modelo')
     lugar_de_trabajo = fields.Many2one(comodel_name='maquinaria.destino')
     trabajo_lineas_ids = fields.One2many(comodel_name='maquinaria.trabajo.linea', inverse_name='maquina_id')
+    no_serie = fields.Char(string='Nro de serie', required=True)
+
+    #Computados
+    ultimo_odometro = fields.Integer(compute='_set_last_data')
+    ultimo_lugar = fields.Many2one(compute='_set_last_data')
+    ultimo_operador = fields.Many2one(comodel_name='res.partner', compute='_set_last_data')
+    ultima_foto_odometro = fields.Binary(compute='_set_last_data')
+
+    @api.multi
+    @api.depends('modelo', 'no_serie')
+    def _set_name(self):
+        for record in self:
+            if record.no_serie and record.modelo:
+                record.name = record.modelo.name + '/ ' + record.no_serie
+
+    @api.multi
+    @api.depends('trabajo_lineas_ids')
+    def _set_last_data(self):
+        for record in self:
+            if record.trabajo_lineas_ids:
+                ultimo_trabajo = record.trabajo_lineas_ids[:-1]
+                record.ultimo_odometro = ultimo_trabajo.odometro
+                record.ultimo_operador = ultimo_trabajo.operador.id
+                record.ultimo_lugar =  ultimo_trabajo.trabajo_destino.id
+                record.ultima_foto_odometro = ultimo_trabajo.odometro_imagen
 
 
 class maquina_marca(models.Model):
@@ -26,13 +53,13 @@ class maquina_modelo(models.Model):
     model_name = fields.Char(string='Modelo')
     marca_id = fields.Many2one(comodel_name='maquinaria.maquina.marca', ondelete='restrict')
 
-
     @api.depends('marca_id', 'model_name')
     @api.multi
     def _get_model_name(self):
         for record in self:
             if record.marca_id and record.model_name:
                 record.name = record.marca_id.name + '/ ' + record.model_name
+
 
 class maquina_destino(models.Model):
     _name = 'maquinaria.destino'
@@ -45,5 +72,7 @@ class maquinaria_trabajo(models.Model):
 
     maquina_id = fields.Many2one(comodel_name='maquinaria.maquina', ondelete='restrict')
     odometro = fields.Integer()
-    odometro_imagen = fields.Binary(string='Odometro')
-    trabajo_destino = fields.Many2one(comodel_name='maquinaria.destino')
+    odometro_imagen = fields.Binary(string='Odometro', attachment=True)
+    trabajo_destino = fields.Many2one(comodel_name='maquinaria.destino', string='Lugar')
+    operador = fields.Many2one(comodel_name='res.partner')
+    fecha_trabajo = fields.Date(default=fields.Date.context_today, string="Fecha")
