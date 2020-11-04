1101702a3fca93dbdbde43f94c8ac44e1b334127
vici0uz <sasorinocho@mixmail.com>
2020-11-04 13:06:21 -0300

    alpha

--- maquinaria/models/maquinaria.py
+++ maquinaria/models/maquinaria.py
@@ -16,15 +16,23 @@ class maquina(models.Model):
 class maquina_marca(models.Model):
     _name = 'maquinaria.maquina.marca'
 
-    name = fields.Char(string='Name')
+    name = fields.Char(string='Name', required=True)
 
 
 class maquina_modelo(models.Model):
     _name = 'maquinaria.maquina.modelo'
 
-    name = fields.Char(string='Name')
-    marca_id = fields.Many2one(comodel_name='maquinaria.maquina.marca')
+    name = fields.Char(string='Name', compute='_get_model_name', store=True)
+    model_name = fields.Char(string='Modelo')
+    marca_id = fields.Many2one(comodel_name='maquinaria.maquina.marca', ondelete='restrict')
 
+
+    @api.depends('marca_id', 'model_name')
+    @api.multi
+    def _get_model_name(self):
+        for record in self:
+            if record.marca_id and record.model_name:
+                record.name = record.marca_id.name + '/ ' + record.model_name
 
 class maquina_destino(models.Model):
     _name = 'maquinaria.destino'
@@ -35,7 +43,7 @@ class maquina_destino(models.Model):
 class maquinaria_trabajo(models.Model):
     _name = 'maquinaria.trabajo.linea'
 
-    maquina_id = fields.Many2one(comodel_name='maquinaria.maquina')
+    maquina_id = fields.Many2one(comodel_name='maquinaria.maquina', ondelete='restrict')
     odometro = fields.Integer()
     odometro_imagen = fields.Binary(string='Odometro')
     trabajo_destino = fields.Many2one(comodel_name='maquinaria.destino')
