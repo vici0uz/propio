80970c30f3559e5014ac19148790fcbd5b34746a
vici0uz <sasorinocho@mixmail.com>
2020-11-26 07:52:09 -0300

    version preliminar migración a odoo11

--- hooks/__manifest__.py
+++ hooks/__manifest__.py
@@ -2,8 +2,8 @@
     'name': 'My module',
     'author': 'Alan Martínez',
     'depends': [
-                'backend_theme_v12',
-                'web_user_shortcut',
+                'backend_theme_v11',
+                # 'web_user_shortcut',
                 'tgl_format_number',
                 'l10n_py_custom',
                 'muk_web_client_refresh'
